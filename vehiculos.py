1fb3e268f831d31f2ec7085a38dab1b16ec4c954
hugosanchez13 <hugosanchez1319@gmail.com>
2026-03-02 13:38:44 +0100

    primer commit

---
+++ vehiculos.py
@@ -0,0 +1,20 @@
+
+class Vehiculo:
+    def __init__(self,matricula,precio_base_dia):
+        self.matricula = matricula
+        #Tarifa estándar de lo que cuesta el alquiler de vehículo
+        self.precio_base_dia = precio_base_dia
+        self.kilometraje_actual = 0
+
+    def calcular_Tarifa(self,dias):
+        tarifa = self.precio_base_dia * dias
+        return tarifa
+
+
+#class Turismo:
+class Furgoneta(Vehiculo):
+    def __init__(self,matricula,precio_base_dia,carga):
+        super().__init__(matricula,precio_base_dia)
+        self.carga = carga
+
+#class Electrico:
