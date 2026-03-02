616f53de2c741cb5f48fd6b6753d3057ea7d3b8c
maximogil <max8112007@gmail.com>
2026-03-02 14:15:41 +0100

    alquiler, devolucion y revision

--- vehiculos.py
+++ vehiculos.py
@@ -7,11 +7,36 @@ class Vehiculo:
         self.kilometraje_actual = 0
         self.km_ultima_revision = 0
         self.disponible = True
+        self.revision = False
 
     def calcular_Tarifa(self,dias):
         tarifa = self.precio_base_dia * dias
         return tarifa
 
+    def alquilar(self):
+        if self.disponible==False and not self.revision:
+            print('Este coche no esta disponible') #Aqui excepcion solape(clase)
+        else:
+            self.disponible=False
+
+    def devolver(self,km_recorridos):
+
+        if self.disponible==True:
+            print('Ya ha sido devuelto') #Aqui excepcion solape(clase)
+        else:
+            self.disponible=True
+            self.kilometraje_actual+=km_recorridos
+
+            if self.kilometraje_actual-self.km_ultima_revision>=15000:
+                print('Necesita hacer una revisión') # Llamar a funcion
+                self.revision = True
+
+    def realizar_revision(self):
+        if self.revision = True:
+            self.revison= False
+            print('Revisioón realizada correctamente')
+        else:
+            print('Este coche no necesita revisión')
 
 #class Turismo:
 class Furgoneta(Vehiculo):
