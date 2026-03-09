da870d6d4f2ff1d7cd2e95ef692226bc6991e4f6
hugosanchez13 <hugosanchez1319@gmail.com>
2026-03-09 14:29:12 +0100

    Creación de las clases Truismo y coche eléctrico y solución de algunos errores.

--- vehiculos.py
+++ vehiculos.py
@@ -1,4 +1,5 @@
 
+
 class Vehiculo:
     def __init__(self,matricula,precio_base_dia):
         self.matricula = matricula
@@ -33,15 +34,44 @@ class Vehiculo:
 
     def realizar_revision(self):
         if self.revision == True:
-            self.revison= False
-            print('Revisioón realizada correctamente')
+            self.revision= False
+            print('Revisión realizada correctamente')
         else:
             print('Este coche no necesita revisión')
 
 #class Turismo:
+class Turismo(Vehiculo):
+    def __init__(self,matricula,precio_base_dia,num_plazas):
+        super().__init__(matricula,precio_base_dia)
+        self.num_plazas = num_plazas # Aquí ponemos una futura restricción para que si pide un coche de 7 plazas no darle una de 5
+    # tarifa distinta, aquí vamos a cobrar más por cada plaza superior a 5, un abono de 5€ por plaza
+    def calcular_Tarifa(self,dias):
+        if self.num_plazas>5:
+            return super().calcular_Tarifa(dias) + ((self.num_plazas - 5)*5)
+
 class Furgoneta(Vehiculo):
     def __init__(self,matricula,precio_base_dia,carga):
         super().__init__(matricula,precio_base_dia)
         self.carga = carga
 
+    def calcular_Tarifa(self, dias):
+        return super().calcular_Tarifa(dias) + ((self.carga / 100) * 2)
+
 #class Electrico:
+
+class Electrico(Vehiculo):
+    def __init__(self, matricula, precio_base_dia, nivel_bateria, autonomia_maxima):
+        super().__init__(matricula, precio_base_dia)
+        self.nivel_bateria = nivel_bateria
+        self.autonomia_maxima = autonomia_maxima
+
+    def alquilar(self):
+        if self.nivel_bateria < 20:
+            print('Error: Batería demasiado baja.')  # Aquí excepción
+
+        else:
+            super().alquilar()
+
+    def cargar(self):
+        self.nivel_bateria = 100
+        print(f'Vehículo {self.matricula} cargado al 100%')
